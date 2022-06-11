70f6e78aa04131ec1617e8595b3fa76ba3e07994
Juan Diego <juandiegomariscal@gmail.com>
2022-06-11 13:33:58 +0200

    feat: añadidas constantes para el tamaño de la red

--- pong.py
+++ pong.py
@@ -4,14 +4,16 @@ class Paleta(pygame.Rect):
     pass
 
 class Pong:
-
-    _ALTO = 200
     _ANCHO = 800
-    _MARGEN_LATERAL = (_ALTO/10)/2
+    _ALTO = 420
     
-    _ALTO_RED = int(_ALTO/15)
+    _MARGEN_LATERAL = (_ALTO/8)/2
+    
     _ANCHO_PALETA = 12
-    _ALTO_PALETA = _ALTO / 5
+    _ALTO_PALETA = _ALTO / 8
+
+    _ALTO_RED = int(_ALTO/14)
+    _ANCHO_RED = _ANCHO_PALETA/2
     def __init__(self):
         pygame.init()
         self.pantalla = pygame.display.set_mode((self._ANCHO, self._ALTO))
@@ -22,26 +24,24 @@ class Pong:
         (self._ALTO - self._ALTO_PALETA)/2, #y
         self._ANCHO_PALETA, #width
         self._ALTO_PALETA) #height
+        self.crear_red()
         '''
         for y in range(int(self._ALTO_RED/2), self._ALTO, self._ALTO_RED*2):
             rectangulo = pygame.Rect(self._ANCHO/2-2, y, 4, self._ALTO_RED)
             pygame.draw.rect(self.pantalla, (255, 255, 255), rectangulo)
         '''
-    def crear_red(self, alto_segmento):
-        for y in range(int(alto_segmento/2), self._ALTO, alto_segmento*2):
-            rectangulo = pygame.Rect(self._ANCHO/2-2, y, self._ANCHO_PALETA, alto_segmento)
+    def crear_red(self):
+        for y in range(int(self._ALTO_RED/2), self._ALTO, self._ALTO_RED*2):
+            rectangulo = pygame.Rect(self._ANCHO/2-2, y, self._ANCHO_RED, self._ALTO_RED)
             pygame.draw.rect(self.pantalla, (255, 255, 255), rectangulo)
         
 
     def bucle_principal(self):
-        
         while True:
-            
             pygame.draw.rect(self.pantalla, (255, 255, 255), self.j1)
             pygame.draw.rect(self.pantalla, (255, 255, 255), self.j2)                   
             pygame.display.flip()
 
 if __name__ == "__main__":
     juego = Pong()
-    juego.crear_red(25)
     juego.bucle_principal()
