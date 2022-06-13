63eac4886c9c96bf44e0df0ed76e101ca1e6f30d
Juan Diego <juandiegomariscal@gmail.com>
2022-06-13 23:03:39 +0200

    fix: Las paletas no se salen de la pantalla

--- pong.py
+++ pong.py
@@ -1,47 +1,53 @@
 import pygame
-    
+ANCHO_PALETA = 10
+ALTO_PALETA = 100
+ANCHO = 800
+ALTO = 400
+MARGEN_LATERAL = (ALTO/6)/2
 class Paleta(pygame.Rect):
-    pass   
+    ARRIBA = True
+    ABAJO = False
 
+    def __init__(self, x, y):
+        super(Paleta, self).__init__(x, y, ANCHO_PALETA, ALTO_PALETA)
+        self.velocidad = 1
+        #super(Paleta, self) equivale a Rect
+        #.__init__(x, y, ANCHO_PALETA, ALTO_PALETA) llama al metodo init de Rect, coge los dos valores de posicion que entran al crear Paleta, y luego metemos las dos constantes globales que hemos creado al principio
+    def mover(self, dir):
+        if dir == self.ARRIBA:
+            print("Muevete hacia arriba")
+            self.y -= self.velocidad
+            if self.y < 0:
+                self.y = 0
+
+        if dir == self.ABAJO:
+            print("Muevete hacia abajo")
+            self.y += self.velocidad
+            if self.y > ALTO-ALTO_PALETA:
+                self.y = ALTO - ALTO_PALETA
 class Pong:
-    _ANCHO = 800
-    _ALTO = 400
     
-    _MARGEN_LATERAL = (_ALTO/6)/2
     
     _ANCHO_PALETA = 12
-    _ALTO_PALETA = _ALTO / 5
+    _ALTO_PALETA = ALTO / 5
 
-    _ALTO_RED = int(_ALTO/12)
+    _ALTO_RED = int(ALTO/12)
     _ANCHO_RED = _ANCHO_PALETA/2
 
     _CRT = True
     def __init__(self):
         pygame.init()
-        self.pantalla = pygame.display.set_mode((self._ANCHO, self._ALTO))
+        self.pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
-        self.j1 = Paleta(self._MARGEN_LATERAL, (self._ALTO - self._ALTO_PALETA)/2, self._ANCHO_PALETA, self._ALTO_PALETA)
+        self.j1 = Paleta(MARGEN_LATERAL, (ALTO - ALTO_PALETA)/2)
 
-        #Partes de un 7 segmentos
-        '''
-        self.a = pygame.Rect(200, self._MARGEN_LATERAL, 50, 8)                  
-        self.b = pygame.Rect(200, self._MARGEN_LATERAL+50, 50, 8)
-        self.c = pygame.Rect(200, self._MARGEN_LATERAL+100, 50, 8)
-        self.d = pygame.Rect(200, self._MARGEN_LATERAL, 8, 50)
-        self.e = pygame.Rect(250-self._ALTO_SEGMENTO, self._MARGEN_LATERAL, 8, 50)
-        '''
-
-        self.j2 = Paleta(
-        self._ANCHO-self._ANCHO_PALETA-self._MARGEN_LATERAL, #x
-        (self._ALTO - self._ALTO_PALETA)/2, #y
-        self._ANCHO_PALETA, #width
-        self._ALTO_PALETA) #height
+        self.j2 = Paleta(ANCHO-ANCHO_PALETA-MARGEN_LATERAL, (ALTO - ALTO_PALETA)/2)
 
         self.crear_red()
         
     def crear_red(self):
-        for y in range(int(self._ALTO_RED/2), self._ALTO, self._ALTO_RED*2):
-            parte_red = pygame.Rect(self._ANCHO/2-2, y, self._ANCHO_RED, self._ALTO_RED)
+        for y in range(int(self._ALTO_RED/2), ALTO, self._ALTO_RED*2):
+            parte_red = pygame.Rect(ANCHO/2-2, y, self._ANCHO_RED, self._ALTO_RED)
             pygame.draw.rect(self.pantalla, (255, 255, 255), parte_red)
         
     def display_seven_segment(self, posx, posy, val):
@@ -76,8 +82,8 @@ class Pong:
                 pygame.draw.rect(self.pantalla, (255, 255, 255), self.g)
     def CRT_filter(self):
         if self._CRT:
-            for y in range(1, self._ALTO, 2):
-                pygame.draw.line(self.pantalla, (0,0,0), [0,y], [self._ANCHO, y])
+            for y in range(1, ALTO, 2):
+                pygame.draw.line(self.pantalla, (0,0,0), [0,y], [ANCHO, y])
         else:
             pass
 
@@ -87,13 +93,23 @@ class Pong:
             for evento in pygame.event.get():
                 if evento.type == pygame.KEYDOWN:
                     if evento.key == pygame.K_ESCAPE:
-                        salir = True
+                        salir = True   
                 if evento.type == pygame.QUIT:
                     salir = True
+            estado_teclas = pygame.key.get_pressed()
+            if estado_teclas[pygame.K_w]:
+                self.j1.mover(Paleta.ARRIBA)
+            if estado_teclas[pygame.K_s]:
+                self.j1.mover(Paleta.ABAJO)
+            if estado_teclas[pygame.K_UP]:
+                self.j2.mover(Paleta.ARRIBA)
+            if estado_teclas[pygame.K_DOWN]:
+                self.j2.mover(Paleta.ABAJO)
+            self.pantalla.fill((0,0,0))
             pygame.draw.rect(self.pantalla, (255, 255, 255), self.j1)
             pygame.draw.rect(self.pantalla, (255, 255, 255), self.j2)
             self.display_seven_segment(200, 20, 5)
-            self.display_seven_segment(self._ANCHO-200, 20, 6)
+            self.display_seven_segment(ANCHO-200, 20, 6)
             self.CRT_filter()
             pygame.display.flip()
 
